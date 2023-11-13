cf4f9ddab34637a7a254b2ee1bf0b1976691412c
Just-Kacper <kacperpaul@icloud.com>
2023-11-13 18:30:23 +0100

    form add

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,12 @@
 "use strict";
 
 function notify() {
-    if ( document.getElementById("log").click && ) {
-        document.getElementById("successfully").innerHTML = "Loged in";
+    // if ( document.getElementById("log").click) {
+    //     document.getElementById("successfully").innerHTML = "Logged in!";
+    //     setTimeout("location.href = 'main.html';",1500);
+    // }
+
+    if (document.getElementById("logpassword") == null && document.getElementById("logmail") == null) {
+        document.getElementById("successfully").innerHTML = "You're not going in";
     }
 }
