6e3a3f2397d7f6be3e85808f03811547d81b9c1a
Just-Kacper <kacperpaul@icloud.com>
2023-11-13 15:49:12 +0100

    First function add

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function notify() {
-    if ( document.getElementById("log").click ) {
+    if ( document.getElementById("log").click && ) {
         document.getElementById("successfully").innerHTML = "Loged in";
     }
 }
