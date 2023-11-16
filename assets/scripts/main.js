bc86a63d62b6a0b7792352d8d741bdadd8de821e
Just-Kacper <kacperpaul@icloud.com>
2023-11-16 20:18:08 +0100

    JavaScript for LogIn add

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,25 @@
 "use strict";
 
-function notify() {
-    // if ( document.getElementById("log").click) {
-    //     document.getElementById("successfully").innerHTML = "Logged in!";
-    //     setTimeout("location.href = 'main.html';",1500);
-    // }
 
-    if (document.getElementById("logpassword") == null && document.getElementById("logmail") == null) {
-        document.getElementById("successfully").innerHTML = "You're not going in";
-    }
+function notification() {
+var emailInput = document.getElementById('emailToLogin').value;
+var passwordInput = document.getElementById('passwordToLogin').value;
+
+// Check if the email contains "@" and has some text before and after it
+if (!/^.+@.+$/.test(emailInput)) {
+  document.getElementById('unsuccessful').innerText = 'Invalid email format. Please enter a valid email address.';
+  document.getElementById('successful').innerText = '';
+  return; // Exit the function if email format is invalid
+}
+
+if (passwordInput) {
+  document.getElementById('successful').innerText = 'Login successful!';
+  document.getElementById('unsuccessful').innerText = '';
+  
+  // Redirect to another site after successful login
+  setTimeout("location.href = 'main.html';",1500);
+} else {
+  document.getElementById('unsuccessful').innerText = 'Login unsuccessful.';
+  document.getElementById('successful').innerText = '';
+}
 }
